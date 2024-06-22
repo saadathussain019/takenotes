7fc1ff0f6f991373fa54aa1bca4f186c1aa115c0
Saadat Hussain <saadathussain019@outlook.com>
2024-06-22 23:09:29 +0500

    step-21

--- lib/views/notes/create_update_note_view.dart
+++ lib/views/notes/create_update_note_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:takenotes/services/auth/auth_service.dart';
+import 'package:takenotes/utilities/dialogs/cannot_share_empty_note_dialog.dart';
 import 'package:takenotes/utilities/generics/get_arguments.dart';
 import 'package:takenotes/services/cloud/cloud_note.dart';
 import 'package:takenotes/services/cloud/firebase_cloud_storage.dart';
@@ -90,6 +92,19 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Write your note'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final text = _textController.text;
+              if (_note == null || text.isEmpty) {
+                await showCannotShareEmptyNoteDialog(context);
+              } else {
+                Share.share(text);
+              }
+            },
+            icon: const Icon(Icons.share),
+          )
+        ],
         backgroundColor: Colors.amberAccent,
       ),
       body: FutureBuilder(
